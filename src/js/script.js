97c4e3d0a74539a95ff5f1f343f56120d4355408
Sashko <sashkoace@gmail.com>
2020-12-13 23:50:29 +0200

    added adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -65,16 +65,22 @@ $('.slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [{
-            breakpoint: 1024,
-            settings: {}
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+            }
         },
         {
-            breakpoint: 600,
-            settings: {}
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+            }
         },
         {
-            breakpoint: 480,
-            settings: {}
+            breakpoint: 439,
+            settings: {
+                slidesToShow: 1,
+            }
         }
     ]
 });
